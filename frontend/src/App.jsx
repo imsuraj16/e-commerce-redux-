34651abe82bac823c7544fb4d2ea967f12f77da6
Surajverma <sv63619@gmail.com>
2025-06-12 12:20:02 +0530

    starting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect } from "react";
-import Mainroutes from "./routes/Mainroutes";
-import Navbar from "./components/Navbar";
-import { currentUser } from "./store/actions/userActions";
-import { useDispatch } from "react-redux";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(currentUser());
-  }, []);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="w-full min-h-screen bg-blue-300 flex flex-col gap-[3rem]">
-      <Navbar />
-      <Mainroutes />
-    </div>
-  );
-};
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
